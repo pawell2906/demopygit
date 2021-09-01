5197f21a254c7b44353a59a7042766d7eb671104
pawell2906 <pokar10@wp.pl>
2021-09-01 14:59:20 +0200

    dodano warunek na sprawdzanie liczby

--- demo.py
+++ demo.py
@@ -1,2 +1,14 @@
-print("hello")
+
+print("Odgadnij liczbe z od 1 do 50")
+liczba = int(input())
+warunek = True
+
+
+while warunek == True:
+
+    if liczba < 0 and liczba >50:
+        print("podales liczbe z poza zakresu. Sprobuj ponownie")
+else:
+       print("czy chesz zagrac ponownie t/n")
+       warunek = input()
 
