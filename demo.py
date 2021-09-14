32e89e0cb3371734e475deefdd87262f05925ab9
pawell2906 <pokar10@wp.pl>
2021-09-14 14:59:13 +0200

    program dziala ale nie obsluguje bledu przy wprowadzaniu np stringu jako liczby wejsciowej

--- demo.py
+++ demo.py
@@ -1,6 +1,7 @@
 
 print("Odgadnij liczbe z od 1 do 50")
 liczba = int(input())
+cel = 13
 
 while True:
 
@@ -8,9 +9,16 @@ while True:
         print("podales liczbe z poza zakresu. Sprobuj ponownie")
         print("Odgadnij liczbe z od 1 do 50")
         liczba = int(input())
-
-
-
-
+    elif liczba > 13 and liczba < 50:
+        print("Troche za duza, wybierz mniejsza")
+        print("Odgadnij liczbe z od 1 do 50")
+        liczba = int(input())
+    elif liczba < 13:
+        print("Troche zad mala, wybierz wieksza")
+        print("Odgadnij liczbe z od 1 do 50")
+        liczba = int(input())
+    elif liczba == cel:
+        print("trafiles , to ta liczba - ", liczba)
+        break
 
 
