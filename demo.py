a72e6fdf55cbefc4a6131ea53a46215869bb6718
pawell2906 <pokar10@wp.pl>
2021-09-14 15:18:16 +0200

    test git revert

--- demo.py
+++ demo.py
@@ -1,7 +1,6 @@
 
 print("Odgadnij liczbe z od 1 do 50")
 liczba = int(input())
-cel = 13
 
 while True:
 
@@ -9,16 +8,9 @@ while True:
         print("podales liczbe z poza zakresu. Sprobuj ponownie")
         print("Odgadnij liczbe z od 1 do 50")
         liczba = int(input())
-    elif liczba > 13 and liczba < 50:
-        print("Troche za duza, wybierz mniejsza")
-        print("Odgadnij liczbe z od 1 do 50")
-        liczba = int(input())
-    elif liczba < 13:
-        print("Troche zad mala, wybierz wieksza")
-        print("Odgadnij liczbe z od 1 do 50")
-        liczba = int(input())
-    elif liczba == cel:
-        print("trafiles , to ta liczba - ", liczba)
-        break
+
+
+
+
 
 
