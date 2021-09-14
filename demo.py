3ae93461f10ea5a6de874bd749a696e023432037
pawell2906 <pokar10@wp.pl>
2021-09-14 12:31:55 +0200

    wreszcie dziala powtarzanie wprowadzenie liczby gdy jet ona z poza zakresu

--- demo.py
+++ demo.py
@@ -1,14 +1,16 @@
 
 print("Odgadnij liczbe z od 1 do 50")
 liczba = int(input())
-warunek = True
 
+while True:
 
-while warunek == True:
-
-    if liczba < 0 and liczba >50:
+    if liczba > 50 or liczba < 0:
         print("podales liczbe z poza zakresu. Sprobuj ponownie")
-else:
-       print("czy chesz zagrac ponownie t/n")
-       warunek = input()
+        print("Odgadnij liczbe z od 1 do 50")
+        liczba = int(input())
+
+
+
+
+
 
